e1219ff9dd4219d6623b38fe8d5dc79b2fef3f50
tlqkfsusdl <126839002+tlqkfsusdl@users.noreply.github.com>
2024-05-02 15:06:29 +0900

    [Update]

--- TextRpg_MonsterHunting/Character/Character.cs
+++ TextRpg_MonsterHunting/Character/Character.cs
@@ -8,7 +8,7 @@ using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
-namespace TextRpg_MonsterHunting
+namespace TextRpg_MonsterHunting //2024.05.02 박재우
 {
     public class Character : Humanoid
 	{
