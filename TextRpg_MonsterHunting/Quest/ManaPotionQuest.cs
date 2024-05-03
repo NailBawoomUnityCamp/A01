fc0f1178415cdd9d14f7d193b009fb2af081c941
ss-zun <zxq0158@gmail.com>
2024-05-03 10:11:38 +0900

    [Fix] 마나 포션 퀘스트 내용 수정

--- TextRpg_MonsterHunting/Quest/ManaPotionQuest.cs
+++ TextRpg_MonsterHunting/Quest/ManaPotionQuest.cs
@@ -19,8 +19,10 @@ namespace TextRpg_MonsterHunting
         public override void QuestContent()
         {
             Console.WriteLine("\n== 마을을 위협하는 미니언 처치 ==");
-            Console.WriteLine("이봐! 마을 근처에 미니언들이 너무 많아졌다고 생각하지 않나? \n마을 주민들의 안전을 위해서라도 저것들 수를 좀 줄여야 한다고!\n모험가인 자네가 좀 처치해주게!");
-            Console.WriteLine("\n\n미니언 5마리 처치 (0/5)");
+            Console.WriteLine("이봐! 마을 근처에 미니언들이 너무 많아졌다고 생각하지 않나? ");
+            Console.WriteLine("마을 주민들의 안전을 위해서라도 저것들 수를 좀 줄여야 한다고!");
+            Console.WriteLine("모험가인 자네가 좀 처치해주게!\n");
+            Console.WriteLine($"미니언 5마리 처치 ({monstersKilledCount}/5)");
             Console.WriteLine("\n보상");
             Console.WriteLine("마나 포션 x1, 5G");
         }
